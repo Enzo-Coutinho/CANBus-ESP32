56fe2fd2a7666c57694f6f7ab4d0e11a15b42082
Enzo-Coutinho <enzo@stemos.com.br>
2025-11-29 17:15:38 -0300

    create project

---
+++ CANBus-ESP32/main/twai_network_example_slave_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_twai.h"
+#include "esp_twai_onchip.h"
+
+void app_main(void)
+{
+
+}
