d11f0144ff076c21edacf39ff44c891ba08faf3e
Enzo-Coutinho <enzo@stemos.com.br>
2025-12-08 21:45:55 -0300

    add struct for can message and ide

--- CANBus-ESP32/main/can-frc/can-frc.h
+++ CANBus-ESP32/main/can-frc/can-frc.h
@@ -12,15 +12,25 @@ enum CAN_MODES {
     DEFAULT
 };
 
-struct can_message_t {
+// 29_bits
+ typedef struct {
+    uint8_t deviceType : 5;
+    uint8_t manufacturer : 8;
+    uint8_t apiClass : 6;
+    uint8_t apiIndex : 4;
+    uint8_t deviceNumber : 6;
+} can_ide_t;
 
-};
+typedef struct {
+    can_ide_t canIde;
+    uint64_t data;
+} can_message_t;
 
 void set_can_mode(enum CAN_MODES mode);
 
 void start_can_bus(const gpio_num_t tx, const gpio_num_t rx);
 
-void send_message(void);
-int read_message();
+void send_message(can_message_t * can_message);
+void read_message(can_message_t * can_message);
 
 #endif
