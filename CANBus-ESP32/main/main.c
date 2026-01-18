c8f100893d19795f666d492da9242b88d798a7c9
Enzo-Coutinho <enzo@stemos.com.br>
2026-01-18 10:28:14 -0300

    save test

--- CANBus-ESP32/main/main.c
+++ CANBus-ESP32/main/main.c
@@ -2,33 +2,11 @@
 #include <stdlib.h>
 #include "can-frc/can-frc.h"
 
-#define TX GPIO_NUM_14
-#define RX GPIO_NUM_13
-
 static const char * MAIN = "MAIN";
 
 void app_main(void)
 {
-    start_can_bus(TX, RX);
-
-    can_message_t can_receive = {0};
-
     for(;;) {
-        read_message(&can_receive);
-        const uint8_t manufacturer = can_receive.can_ide.manufacturer;
-        const uint8_t deviceType = can_receive.can_ide.device_type;
-        const uint8_t apiClass = can_receive.can_ide.api_class;
-        const uint8_t apiIndex = can_receive.can_ide.api_index;
-        ESP_LOGI(MAIN, "Manufacturer: %s", can_receive.can_ide.manufacturer);
-        ESP_LOGI(MAIN, "Device Type: %s", can_receive.can_ide.device_type); 
-        ESP_LOGI(MAIN, "API Class: %d", can_receive.can_ide.api_class);
-        ESP_LOGI(MAIN, "API Index: %d", can_receive.can_ide.api_index);
-        ESP_LOGI(MAIN, "Device Number: %d", can_receive.can_ide.device_number);
-        ESP_LOGI(MAIN, "Data lenght: %d", can_receive.dlc);
-        ESP_LOGI(MAIN, "Is Error: %d", can_receive.is_error);
-        ESP_LOGI(MAIN, "Is CAN FD: %d", can_receive.is_fd_format);
-        uint64_t data = 0;
-        for(uint8_t i = 0; i<can_receive.dlc; i++)
-            data |= can_receive.data[i];
+       ESP_LOGI(MAIN, "%d", 0x01234567890123456789ABCDEF012345);
     }
 }
